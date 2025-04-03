4c31f62e7809e742ad5e7f1bbfa7b9219047237c
Ruben Marinez <me@rubenmarinez.com>
2025-04-03 12:42:45 -0600

    initial structureand files

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+
+
+// https://astro.build/config
+export default defineConfig({
+  devToolbar: {
+    enabled: true,
+  },
+
+    vite: {
+      plugins: [tailwindcss()],
+    },
+  });
+  
