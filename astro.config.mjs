d6a9a6f6d8b55438d74d4017612177c57445d45a
Ruben Marinez <me@rubenmarinez.com>
2025-04-04 15:27:39 -0600

    add hero section

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
-    enabled: true,
+    enabled: false,
   },
 
     vite: {
